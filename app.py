d92984e5b42bdeb615ad5e2963eecc38f373be4a
Bhanunagesh <108947126+Bhanunagesh@users.noreply.github.com>
2026-04-05 23:41:40 +0530

    Update model to use gemini-1.5-flash-latest

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.set_page_config(page_title="Conscious Bridge Labs", layout="centered")
 # Gemini setup
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 #model = genai.GenerativeModel("gemini-pro")
-model = genai.GenerativeModel("gemini-1.5-pro")
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 # ---------------- BRANDING ----------------
 st.markdown("""
